a3e2da7d47995c0dfcbfba2f980dbca66f2b0b4e
KOSTIANTYN SHELKOVENKO <kos8704@ukr.net>
2023-03-12 00:36:25 +0200

    second task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = createGalleryMarkup();
+
+function createGalleryMarkup() {
+    return galleryItems.map(({ preview , original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    }).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(() => {
+  player.getCurrentTime().then((seconds) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+  });
+}, 1000));
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
